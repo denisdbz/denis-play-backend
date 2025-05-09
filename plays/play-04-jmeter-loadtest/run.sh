d7df92a1d5aff4688468ae692ad5f0798823fcab
denisdbz <dns.gtv@gmail.com>
2025-05-09 00:42:26 -0300

    atualiza run.sh do play - 04

--- plays/play-04-jmeter-loadtest/run.sh
+++ plays/play-04-jmeter-loadtest/run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 echo "[*] Iniciando teste de carga com JMeter..."
-jmeter -n -t plano-teste.jmx -l resultados.jtl
+jmeter -n -t teste-carga.jmx -l resultados.jtl
 echo "[✓] Teste com JMeter finalizado. Logs salvos em resultados.jtl."
