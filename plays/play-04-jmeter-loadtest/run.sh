fc4fe80182b5b8293b503ad7ba94d5b03fe179d0
denisdbz <dns.gtv@gmail.com>
2025-05-14 22:46:07 -0300

    atualiza teste-carga.jmx do play-04

--- plays/play-04-jmeter-loadtest/run.sh
+++ plays/play-04-jmeter-loadtest/run.sh
@@ -1,4 +1,19 @@
 #!/bin/bash
 echo "[*] Iniciando teste de carga com JMeter..."
+sleep 1
+
+if [[ ! -f teste-carga.jmx ]]; then
+  echo "[X] Arquivo teste-carga.jmx não encontrado."
+  exit 1
+fi
+
+# Executa o teste
 jmeter -n -t teste-carga.jmx -l resultados.jtl
-echo "[✓] Teste com JMeter finalizado. Logs salvos em resultados.jtl."
+
+# Verifica se a execução ocorreu com sucesso
+if [[ $? -eq 0 ]]; then
+  echo "[✓] Teste com JMeter finalizado. Logs salvos em resultados.jtl."
+  echo "[✓] Teste finalizado com sucesso."
+else
+  echo "[X] Erro ao executar o JMeter."
+fi
